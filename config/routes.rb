6ad7d4524b1a7a3495ee0590f55c83e16a5847fb
Freddymorato <Freddymorato@gmail.com>
2018-10-30 13:02:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'posts#index'
+
   resources :authors, only: [:show]
   resources :posts, except: [:delete]
+
   get 'posts/:id/post_data', to: 'posts#post_data'
 end
