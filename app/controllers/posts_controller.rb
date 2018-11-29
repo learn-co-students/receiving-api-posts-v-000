407b3369073f168acdfb3b7ec084a9309507f259
github username <github email address>
2018-11-28 23:17:00 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    render json: @post, status: 201
   end
 
   def edit
@@ -32,9 +32,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
+    render json: PostSerializer.serialize(post)
   end
 
 private
