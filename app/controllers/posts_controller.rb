e0b659e5420004859251d33deeecb43783960d75
Jayon Kim <jpkim921@gmail.com>
2019-01-05 14:58:13 -0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
+
+    # use to render json instead of redirect
+    render json: @post, status: 201
+
   end
 
   def edit
