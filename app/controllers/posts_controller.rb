892630bccf441e7027940dff571b7446bc430f34
Kachel Wittig <kjwittig@gmail.com>
2019-01-20 19:36:57 -0600

    donezo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: %i[show edit update]
 
   def index
     @posts = Post.all
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post}
+      format.json { render json: @post }
     end
   end
 
@@ -19,11 +19,10 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    render json: @post, status: 201
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update(post_params)
@@ -32,12 +31,13 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
+    # render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: %i[title description id],
+                              include: [author: { only: [:name] }])
   end
 
-private
+  private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
